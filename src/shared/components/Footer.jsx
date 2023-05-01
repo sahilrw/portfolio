dc143f69175cd290283fad2526771ee2443244fc
Bjørn Olav Gjørven <bjorn.gjorven@iterate.no>
2023-05-01 17:08:46 +0200

    Change hover color in footer to correspond with rest of site

--- src/shared/components/Footer.jsx
+++ src/shared/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-black text-white w-full py-10 px-6">
+    <footer className="bg-black text-white w-full py-32 px-6">
       <div className="container mx-auto max-w-4xl">
         <div className="flex justify-between items-center">
           <div className="mb-2 md:mb-0">
@@ -14,21 +14,21 @@ const Footer = () => {
               href="https://www.linkedin.com/in/sahilrw/"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-white hover:text-teal-300 mr-4">
+              className="text-white hover:text-lime-300 mr-4">
               LinkedIn
             </a>
             <a
               href="https://twitter.com/_sahil_r"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-white hover:text-teal-300 mr-4">
+              className="text-white hover:text-lime-300 mr-4">
               Twitter
             </a>
             <a
               href="https://github.com/SahilRw"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-white hover:text-teal-300">
+              className="text-white hover:text-lime-300">
               GitHub
             </a>
           </div>
