3150b7aeda781298347be29d2783b7de5b9d0fd4
Bjørn Olav Gjørven <bjorn.gjorven@iterate.no>
2023-05-01 13:15:25 +0200

    Add favicon to project for all major browsers & change page title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import LandingPage from './pages/LandingPage';
+
 function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return <LandingPage />;
 }
 
 export default App;
