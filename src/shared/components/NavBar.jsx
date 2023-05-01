a8702d7aca395cb007082b05351bceb369fbd9bf
Bjørn Olav Gjørven <bjorn.gjorven@iterate.no>
2023-05-01 15:16:11 +0200

    make navbar sticky and reduce its size onScroll

--- src/shared/components/NavBar.jsx
+++ src/shared/components/NavBar.jsx
@@ -1,10 +1,29 @@
+import React, { useEffect, useRef } from 'react';
 import scrollToElement from '../utility/scrollToElement';
 
 const Navbar = () => {
+  const navbarRef = useRef();
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        navbarRef.current.classList.add('scrolled');
+      } else {
+        navbarRef.current.classList.remove('scrolled');
+      }
+    };
+
+    window.addEventListener('scroll', onScroll);
+
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-black p-6">
+    <nav ref={navbarRef} className="navbar fixed top-0 w-full flex items-center justify-between flex-wrap bg-black p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-12">
-        <span className="font-semibold text-xl tracking-tight">My Portfolio</span>
+        <span className="font-semibold text-xl tracking-tight">Sahil Dev</span>
       </div>
       <div className="flex items-center justify-end">
         <div className="text-sm">
