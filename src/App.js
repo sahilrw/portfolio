2c3fcba1c2832ee9e90c5216848979df2eaf90d4
Bjørn Olav Gjørven <bjorn.gjorven@iterate.no>
2023-05-01 13:50:07 +0200

    rename LandingPage to Landing to solve stupid OSX casing bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import LandingPage from './pages/LandingPage';
+import LandingPage from './pages/Landing';
 import Navbar from './shared/components/NavBar';
 import Footer from './shared/components/Footer';
 
