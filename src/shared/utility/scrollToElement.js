2cae41af0e9273f48b7c4e032ad5a4a9ea284961
Bjørn Olav Gjørven <bjorn.gjorven@iterate.no>
2023-05-01 15:21:56 +0200

    Add offset to scrollIntoView function so that navbar doesnt overlap with content

--- src/shared/utility/scrollToElement.js
+++ src/shared/utility/scrollToElement.js
@@ -1,9 +1,17 @@
-const scrollToElement = (elementId) => {
+const scrollToElement = (elementId, offset = 150) => {
   // Find the element you want to scroll to
-  const aboutSection = document.getElementById(elementId); //example: elementId = "about"
+  const element = document.getElementById(elementId);
 
-  // Scroll to the element
-  aboutSection.scrollIntoView({ behavior: 'smooth' });
+  // Calculate the y position to scroll to with the offset
+  const y = element.getBoundingClientRect().top + window.pageYOffset - offset;
+
+  // Scroll to the element with the offset
+  window.scroll({
+    top: y,
+    behavior: 'smooth',
+    block: 'start',
+    inline: 'nearest'
+  });
 };
 
 export default scrollToElement;
