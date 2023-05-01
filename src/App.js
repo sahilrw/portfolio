fb12998f86a9405eb93ec0e691fc314bcaae4a5c
Bjørn Olav Gjørven <bjorn.gjorven@iterate.no>
2023-05-01 13:43:28 +0200

    Include navbar and footer in App.jsx, add comments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import LandingPage from './pages/LandingPage';
+import Navbar from './shared/components/NavBar';
+import Footer from './shared/components/Footer';
 
 function App() {
-  return <LandingPage />;
+  return (
+    // The outer div is a flex container arranged vertically (flex-col)
+    // and is always at least the height of the screen (min-h-screen)
+    // The main section takes up all available space (flex-grow)
+    // Cool! This is a great way to make sure the footer is always at the bottom
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <LandingPage />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
