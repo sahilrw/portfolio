23e040de8f8a02ca7f5eae5bb17310aa8d483f13
Sahil R <sahilrw000@gmail.com>
2023-05-02 15:23:32 +0530

    added margin under the two  elements

--- src/pages/LandingPage/EmailConfirm.jsx
+++ src/pages/LandingPage/EmailConfirm.jsx
@@ -8,7 +8,7 @@ const EmailConfirm = ({email, message}) => {
         <br />
         <div className='bg-gray-800 p-6 rounded-lg'>
             <h3 className="text-xl font-bold mb-4">Here is a copy of your email: </h3>
-            <p className='my-2'>
+            <p className='mb-2'>
                 <span className="font-bold">Email Address:</span> {email}
             </p>
             <p>
