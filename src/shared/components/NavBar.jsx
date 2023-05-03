0785cde4592ad7a22dc67ba2cc25217faabf45ba
Bjørn Olav Gjørven <bjorn.gjorven@iterate.no>
2023-05-03 19:54:52 +0200

    Test logo in navbar

--- src/shared/components/NavBar.jsx
+++ src/shared/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import scrollToElement from '../utility/scrollToElement';
-// import Logo from '../icons/Logo';
+import Logo from '../icons/Logo';
 
 const Navbar = () => {
   const navbarRef = useRef();
@@ -29,7 +29,9 @@ const Navbar = () => {
     <nav
       ref={navbarRef}
       className="navbar fixed top-0 w-full flex items-center justify-between flex-wrap bg-black p-6 z-20">
+      {/* Make space by adding flex-gap to the div below, find the tailwind class for it and put it in the className attribute--> */}
       <div className="flex items-center flex-shrink-0 text-white mr-12">
+        <Logo />
         <button onClick={() => scrollToElement('home')} className="font-semibold text-xl tracking-tight">
           Sahil Dev
         </button>
