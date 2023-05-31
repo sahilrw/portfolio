7b187a166321355752b2d3358992b2723195d4b3
Sahil R <sahilrw000@gmail.com>
2023-05-31 14:51:28 +0530

    added the weather app

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -110,15 +110,6 @@ const LandingPage = () => {
               </PortfolioCard>
 
               <PortfolioCard
-                title="Review cards"
-                imageSrc="projectReview.jpg"
-                imgAltTxt="Preview image of review cards project"
-                clickUrl="https://leafy-bavarois-d0b9f1.netlify.app/">
-                Fully responsive webpage made using HTML, CSS and JavaScript. This webpage displays a collection of
-                images and descriptions of reviews.
-              </PortfolioCard>
-
-              <PortfolioCard
                 title="Weather App"
                 imageSrc="weather-app.png"
                 imgAltTxt="Preview image of weather app project"
